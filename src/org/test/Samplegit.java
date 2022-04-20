f7e184d5ad5489c7c98a32a73c3cdbb09b5fb0ba
RajeshwariBRr <rajeshwari.raj78@gmail.com>
2022-04-20 22:31:55 +0530

    Project structure

---
+++ src/org/test/Samplegit.java
@@ -0,0 +1,13 @@
+package org.test;
+
+public class Samplegit {
+public static void main(String[] args) {
+	
+	System.out.println("text1");
+	System.out.println("text2");
+	
+	System.out.println("text3");
+	
+}
+
+}
